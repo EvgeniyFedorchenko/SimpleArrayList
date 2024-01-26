c58b9db5841cd04113600502b22692d1d28d6679
EvgeniyFedorchenko <jecky432@gmail.com>
2024-01-27 02:48:08 +0300

    add adding methods (beta)

--- src/main/java/com/evgeniyfedorchenko/simplearraylist/Main.java
+++ src/main/java/com/evgeniyfedorchenko/simplearraylist/Main.java
@@ -1,8 +1,32 @@
 package com.evgeniyfedorchenko.simplearraylist;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        SimpleArrayList myList = new SimpleArrayList(10);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("q");
+        myList.add("0");
+        myList.add("1");
+        myList.add("2");
+        myList.add("3");
+        myList.add("4");
+        myList.add("5");
+        myList.add("6");
+        myList.add("7");
+        myList.add("8");
+        myList.add("9");
+        myList.add("10");
+        myList.add("11");
+
+        myList.add(4, "999");
+        myList.add("12");
+        myList.add("13");
+        myList.add("14");
+        myList.add(8, "999");
+        System.out.println(myList);
     }
 }
--- src/main/java/com/evgeniyfedorchenko/simplearraylist/SimpleArrayList.java
+++ src/main/java/com/evgeniyfedorchenko/simplearraylist/SimpleArrayList.java
@@ -2,75 +2,133 @@ package com.evgeniyfedorchenko.simplearraylist;
 
 import com.evgeniyfedorchenko.simplearraylist.interfaces.StringList;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 public class SimpleArrayList implements StringList {
 
-	@Override
-	public String add(String item) {
-		return null;
-	}
+    private String[] innerArray;
+    private static final int DEFAULT_SIZE = 10;
+    private int size;
 
-	@Override
-	public String add(int index, String item) {
-		return null;
-	}
+    public SimpleArrayList() {
+        this(DEFAULT_SIZE);
+    }
 
-	@Override
-	public String set(int index, String item) {
-		return null;
-	}
+    public SimpleArrayList(int initialSize) {
+        size = 0;
+        this.innerArray = new String[initialSize];
+    }
 
-	@Override
-	public String remove(String item) {
-		return null;
-	}
+    public SimpleArrayList(Collection<String> sourceCollection) {
+        size = sourceCollection.size();
+        this.innerArray = sourceCollection.toArray(new String[0]);
+    }
 
-	@Override
-	public String remove(int index) {
-		return null;
-	}
+    @Override
+    public String add(String item) {
+        String[] local = innerArray;
 
-	@Override
-	public boolean contains(String item) {
-		return false;
-	}
+        if (size - 1 < innerArray.length) {
+            for (int i = 0; i < local.length; i++) {
+                if (local[i] == null) {
+                    local[i] = item;
+                    size += 1;
+                    return item;
+                }
+            }
+        }
+        int oldSize = size;
+        boostSize();
+        local[oldSize - 1] = item;
+        return item;
+    }
 
-	@Override
-	public int indexOf(String item) {
-		return 0;
-	}
+    private void boostSize() {
+        String[] newInnerArray = new String[size + size / 2];
+        System.arraycopy(innerArray, 0, newInnerArray, 0, size);
+        innerArray = newInnerArray;
+        size += size / 2;
+    }
 
-	@Override
-	public int lastIndexOf(String item) {
-		return 0;
-	}
+    @Override
+    public String add(int index, String item) {
+        if (index >= innerArray.length || index < 0) {
+            throw new RuntimeException();
+        }
+        String[] local = innerArray;
+        if (local[size - 1] == null) {
+            String[] turnedEls = Arrays.copyOfRange(local, index, size - 1);
+            local[index] = item;
+            System.arraycopy(turnedEls, 0, local, index + 1, turnedEls.length);
+            return item;
+        }
+        boostSize();
+        return add(index, item);
+    }
 
-	@Override
-	public String get(int index) {
-		return null;
-	}
+    @Override
+    public String set(int index, String item) {
+        return null;
+    }
 
-	@Override
-	public boolean equals(StringList otherList) {
-		return false;
-	}
+    @Override
+    public String remove(String item) {
+        return null;
+    }
 
-	@Override
-	public int size() {
-		return 0;
-	}
+    @Override
+    public String remove(int index) {
+        return null;
+    }
 
-	@Override
-	public boolean isEmpty() {
-		return false;
-	}
+    @Override
+    public boolean contains(String item) {
+        return false;
+    }
 
-	@Override
-	public void clear() {
+    @Override
+    public int indexOf(String item) {
+        return 0;
+    }
 
-	}
+    @Override
+    public int lastIndexOf(String item) {
+        return 0;
+    }
 
-	@Override
-	public String[] toArray() {
-		return new String[0];
-	}
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean equals(StringList otherList) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public String[] toArray() {
+        return new String[0];
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(innerArray);
+    }
 }
